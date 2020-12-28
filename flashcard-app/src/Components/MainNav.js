08a1a60d243573cd691ab5f417b636e08512cff5
phuocdh <huuphuoc97it@gmail.com>
2020-12-28 18:54:17 +0700

    add community

--- flashcard-app/src/Components/MainNav.js
+++ flashcard-app/src/Components/MainNav.js
@@ -1,6 +1,6 @@
 import '../style.css'
 import React from 'react'
-import {BrowserRouter as Route,Switch,Redirect,BrowserRouter,Link } from 'react-router-dom'
+import {BrowserRouter as Route,Switch,Redirect,BrowserRouter,NavLink } from 'react-router-dom'
 
 import Community from './Community'
 
@@ -14,19 +14,19 @@ export default function MainNav(){
         <BrowserRouter>
             <nav className="main-nav">
                 <ul>
-                    <li><a href="#" id="menu" onClick={menuClick}><i className="material-icons md-24">menu</i></a></li>
-                    <li><a href="/"><i className="material-icons md-24">home</i></a></li>
-                    <li><Link to="/community"><span>Cộng đồng</span></Link></li>
-                    <li><Link to="/introduce"><span>Giới thiệu</span></Link></li>
-                    <li><Link to="/help"><span>Trợ giúp</span></Link></li>
+                    <li><a href="#" id="menu" onClick={menuClick}><i className="material-icons md-30">menu</i></a></li>
+                    <li><a href="/"><i className="material-icons md-30">home</i></a></li>
+                    <li><NavLink to="/community"><span>Cộng đồng</span></NavLink></li>
+                    <li><NavLink to="/introduce"><span>Giới thiệu</span></NavLink></li>
+                    <li><NavLink to="/help"><span>Trợ giúp</span></NavLink></li>
                 </ul>
             </nav>
-            <main>
+            {/* <main>
                 <Switch>
-                    <Route path="/community" component={Community} />
+                    <Route path="/community" component={Community}></Route>
                 </Switch>
                 <Redirect to="/"/>
-            </main>
+            </main> */}
         </BrowserRouter>
     </div>
     )
