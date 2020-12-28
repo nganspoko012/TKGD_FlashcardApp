4300ce58147aecb5e08c6072271e0fb38fdac04a
phuocdh <huuphuoc97it@gmail.com>
2020-12-28 13:07:20 +0700

    fix lại navbar và test route

--- flashcard-app/src/Components/MainNav.js
+++ flashcard-app/src/Components/MainNav.js
@@ -1,4 +1,8 @@
 import '../style.css'
+import React from 'react'
+import {BrowserRouter as Route,Switch,Redirect,BrowserRouter,Link } from 'react-router-dom'
+
+import Community from './Community'
 
 export default function MainNav(){
     function menuClick(e) {
@@ -6,12 +10,24 @@ export default function MainNav(){
         console.log("Menu clicked!")
     }
     return (
-    <nav className="main-nav">
-        <a href="#" id="menu" onClick={menuClick}><i className="material-icons md-24">menu</i></a>
-        <a href="#"><i className="material-icons md-24">home</i></a>
-        <a href="#"><span>Cộng đồng</span></a>
-        <a href="#"><span>Giới thiệu</span></a>
-        <a href="#"><span>Trợ giúp</span></a>
-    </nav>
-)
+    <div class="nav-bar">
+        <BrowserRouter>
+            <nav className="main-nav">
+                <ul>
+                    <li><a href="#" id="menu" onClick={menuClick}><i className="material-icons md-24">menu</i></a></li>
+                    <li><a href="/"><i className="material-icons md-24">home</i></a></li>
+                    <li><Link to="/community"><span>Cộng đồng</span></Link></li>
+                    <li><Link to="/introduce"><span>Giới thiệu</span></Link></li>
+                    <li><Link to="/help"><span>Trợ giúp</span></Link></li>
+                </ul>
+            </nav>
+            <main>
+                <Switch>
+                    <Route path="/community" component={Community} />
+                </Switch>
+                <Redirect to="/"/>
+            </main>
+        </BrowserRouter>
+    </div>
+    )
 }
