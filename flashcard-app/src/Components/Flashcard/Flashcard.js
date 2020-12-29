76d0dba84e2bb18de075ed94a9654ab201313e89
nganspoko012 <59394926+nganspoko012@users.noreply.github.com>
2020-12-29 12:20:35 +0700

    Add layout learn screen

--- flashcard-app/src/Components/Flashcard/Flashcard.js
+++ flashcard-app/src/Components/Flashcard/Flashcard.js
@@ -5,6 +5,8 @@ export default function Flashcard({ flashcard = [], flip = false, flashcardClick
     return (
         <div className="flashcard" onClick={flashcardClick}>
             <div className={flip === false ? "flashcard-wrapper" : "flashcard-wrapper flip"}>
+                <div className="prev-button"><i className="material-icons md-48">arrow_back_ios</i></div>
+                <div className="next-button"><i className="material-icons md-48">arrow_back_ios</i></div>
                 <Fields style="flashcard-front" fields={flashcard.frontFields}></Fields>
                 <Fields style="flashcard-back" fields={flashcard.backFields}></Fields>
             </div>
